ffa0136cadfdfc0b9ddee115a983c7d4a7fcd537
jhonristh <jhonreyoquendo01@gmail.com>
2026-07-01 20:54:30 +0800

    irrelevant deletion

--- backend0/stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping SmartRoute PH...
-taskkill /f /im uvicorn.exe >nul 2>&1
-taskkill /f /im python.exe >nul 2>&1
-taskkill /f /im node.exe >nul 2>&1
-echo All services stopped!
